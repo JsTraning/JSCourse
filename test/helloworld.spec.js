d47a72e7f681530f1296ade768967eacbaae0e75
Ivanovski <ivanovski.vital@gmail.com>
2019-04-22 10:22:53 +0300

    created branch after first lesson for homework

---
+++ test/helloworld.spec.js
@@ -0,0 +1,8 @@
+const { descride, it } = require('mocha');
+const logger = require('../utils/log.util');
+
+describe('Hello World Test Suite', () => {
+    it('should write "Hello World"', () => {
+        logger.info('Hello World');
+    });
+});
